68a98e0c8565da7b11d8ef1a0e6bb4c5966e4802
Matheus Pazinati <matheuspazinati@gmail.com>
2022-10-12 06:13:51 -0300

    Adiciona ferramenta Storybook ao projeto para documentar os componentes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,6 @@ import './styles/main.css'
 
 export function App() {
   return (
-    <h1 className='text-5xl font-bold text-gray-900'>Hello World</h1>
+    <h1 className='text-5xl font-bold text-gray--800'>Hello World</h1>
   )
 }

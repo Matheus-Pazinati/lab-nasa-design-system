e5b2aa9f373003e8c99527ae3742a4b8343ca3f0
Matheus Pazinati <matheuspazinati@gmail.com>
2022-10-12 06:54:58 -0300

    Cria primeiro componente e documenta no Storybook

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,26 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from "./Text";
+
+export default {
+  title: 'Components/Text',
+  component: Text,
+  args: {
+    children: 'Text',
+  }
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {
+  args: {
+    size: 'md'
+  }
+}
+export const Small: StoryObj<TextProps> = {
+  args: {
+    size: 'sm'
+  }
+}
+export const Large: StoryObj<TextProps> = {
+  args: {
+    size: 'lg'
+  }
+}

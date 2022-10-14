cf41f095cc59e6a3aae2561b631c0e2eeb4ac9b1
Matheus Pazinati <matheuspazinati@gmail.com>
2022-10-14 04:36:08 -0300

    Criando action no github para fazer depoloy automatico do Storybook

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/lab-nasa-design-system/'
+    }
+
+    return config
   }
 }

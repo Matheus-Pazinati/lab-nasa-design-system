ba70a7b55b3e31576e7ff2f4576b6afced9ed492
Matheus Pazinati <matheuspazinati@gmail.com>
2022-10-14 05:48:28 -0300

    Adiciona addon do Storybook para aplicar testes de acessibilidade nos componentes

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -7,7 +7,7 @@ export function Checkbox() {
     <CheckboxPrimitive.Root
       className='w-6 h-6 bg-gray-800 rounded flex items-center justify-center p-[2px]'
     >
-      <CheckboxPrimitive.Indicator>
+      <CheckboxPrimitive.Indicator asChild>
         <Check weight='bold' className='flex-1 text-blue-500 w-5 h-5' />
       </CheckboxPrimitive.Indicator>
     </CheckboxPrimitive.Root>
